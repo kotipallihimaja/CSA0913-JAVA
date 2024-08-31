b9bbf4452fc4ffc8fa88533ca97fcdb0287b1b0f
kotipallihimaja <141385009+kotipallihimaja@users.noreply.github.com>
2024-08-31 10:13:24 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        String str="Welcome";
+        Scanner sc=new Scanner(System.in);
+        System.out.println(str.length());
+        sc.close();
+    }
+}
+/*import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        String str="Welcome";
+        System.out.println(str);
+        String s=new String("Java Programing");
+        System.out.println(s);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string");
+        String s2=sc.nextLine();
+        System.out.println(s2);
+    }
+}*/

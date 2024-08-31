6fa8e09effe74bbe5bd5fda32a19e2989239fe91
kotipallihimaja <141385009+kotipallihimaja@users.noreply.github.com>
2024-08-31 09:53:14 +0530

    Add files via upload

---
+++ copystring.java
@@ -0,0 +1,9 @@
+public class copystring {
+    public static void main(String[] args) {
+        String original="Hello World";
+        String copy=original;
+        System.out.println("original String:"+original);
+        System.out.println("Copied String:"+copy);
+    }
+}
+

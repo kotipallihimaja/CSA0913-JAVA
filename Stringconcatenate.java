6fc7e410c47cf48ac6aa35bd04f45c0aeb560976
kotipallihimaja <141385009+kotipallihimaja@users.noreply.github.com>
2024-08-31 10:11:32 +0530

    Add files via upload

---
+++ Stringconcatenate.java
@@ -0,0 +1,8 @@
+public class Stringconcatenate {
+    public static void main(String[] args) {
+        String firstString = "Pragna ";
+        String secondString = "Sree!";
+        String concatenatedString = firstString + secondString;
+        System.out.println("Concatenated String: " + concatenatedString);
+    }
+}
